9854a100d4735ee8941e97cc2a3140457c930772
anirudhatch <anirudh@circuithouse.tech>
2024-11-25 17:40:05 +0530

    change version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.smitcoderx.learn.androidci_cd"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.smitcoderx.learn.androidci_cd"
