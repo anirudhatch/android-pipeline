28a3b4d12f8700e485141a30142bf0bd64f3b8ac
anirudhatch <anirudh@circuithouse.tech>
2024-11-28 18:50:18 +0530

    added

--- app/src/main/java/com/smitcoderx/learn/androidci_cd/MainActivity.kt
+++ app/src/main/java/com/smitcoderx/learn/androidci_cd/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -20,10 +21,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidCICDTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Test Android CI CD Testing",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column {
+                        Greeting(
+                            name = "Test Android CI CD Testing",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                        Greeting(
+                            name = "Test Android CI CD Testing",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
